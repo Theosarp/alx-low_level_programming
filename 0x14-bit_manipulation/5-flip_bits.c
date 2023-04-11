32e0502b1728e13b83ca5ff824377a6c5d251375
Theo Akesseh Sarpong <theo.sarp@yahoo.com>
2023-04-10 17:06:42 -0700

    Number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip
+ * flip_bits - returns number of bits you would need to flip
  * to get from one number to another
  * @n: first number
  * @m: second number
@@ -10,17 +10,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int x, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+	unsigned int x;
 
-	for (x = 98; x >= 0; x--)
+	for (x = 0; n || m; n >>= 1, m >>= 1)
 	{
-		current = exclusive >> x;
-		if (current & 1)
-			count++;
+		if ((n & 1) != (m & 1))
+			x++;
 	}
 
-	return (count);
+	return (x);
 }
+
 
